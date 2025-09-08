1fc99dd7577f77c9e6e4f659bd160f7ef6bcc89a
dongesz <majormate12@gmail.com>
2025-09-08 11:50:36 +0200

    feat: create "Szemely" class
    - Create szemely class
    - Create Name and Age class variables
    - Instantiate new szemely, output the properties

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace OOPalapok
 {
+    public class Szemely
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely { Name = "peti", Age = 20 };
+            Console.WriteLine($"A szemely neve: {person.Name}");
+            Console.WriteLine($"Eletkora: {person.Age}");
         }
     }
 }
