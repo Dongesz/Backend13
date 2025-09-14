e8f93900f3e9e5b568021b0bb09c1cf0c2d1cad2
Dongesz <majormate12@gmail.com>
2025-09-14 15:40:37 +0200

    feat: feladat3 - Validate age via Property
    - ensures that the age cannot be negative when creating a Szemely

---
+++ 2025.09.08/OOPalapok/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+    public class Szemely
+    {
+        private string _name;
+        private int _age;
+        
+        public int Age
+        {
+            get => _age;
+            set => _age = value < 0 ? 0 : value;
+
+        }
+        public Szemely(string name, int age)
+        {
+            _name = name;
+            Age = age;
+        }
+        
+        public string kiir()
+        {
+            return $"A szemely neve: {_name} \nEletkora: {Age}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szemely person = new Szemely("Peti", 14);
+            Console.WriteLine(person.kiir());
+        }
+    }
+}
