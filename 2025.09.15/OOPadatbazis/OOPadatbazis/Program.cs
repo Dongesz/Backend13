c48399f7749a34a75287267c112fc86c11b67993
dongesz <majormate12@gmail.com>
2025-09-15 10:22:19 +0200

    chore(start): OOPadatbazis task
    - initialize solution for OOPadatbazis

---
+++ 2025.09.15/OOPadatbazis/OOPadatbazis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPadatbazis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
