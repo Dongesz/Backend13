dc38ddf371d0a34e6b6d86cf06cf1910f4ac35b7
Majoros Máté Martin <MajorosMM@kkszki.local>
2025-11-10 09:00:35 +0100

    feat: initial commit for bloggers app

---
+++ 2025.11.10/BlogApi/BlogApi/Model/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BlogApi.Model
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+
+        }
+        public BlogDbContext(DbContextOptions options) :base(options) 
+        {
+
+        }
+    }
+}
