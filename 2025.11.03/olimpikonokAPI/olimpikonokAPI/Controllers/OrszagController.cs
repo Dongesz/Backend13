155d4bbc24618d5e76eebb2a0602f4db606335ac
Majoros Máté Martin <MajorosMM@kkszki.local>
2025-11-10 08:59:57 +0100

    feat: 2025.11.03 - gyakorlas

---
+++ 2025.11.03/olimpikonokAPI/olimpikonokAPI/Controllers/OrszagController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using olimpikonokAPI.Models;
+
+namespace olimpikonokAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrszagController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (var context = new OlimpikonokContext())
+            {
+                try
+                {
+                    var orszagok = context.Orszags.ToList();
+                    return Ok(orszagok);
+                }
+                catch (Exception x)
+                {
+
+                    List<Orszag> orszagok = new List<Orszag>();
+                    Orszag hiba = new Orszag()
+                    {
+                        Id = -1,
+                        Nev = $"Hiba az adatok betoltese kozben: {x.Message}"
+                    };
+                    orszagok.Add(hiba);
+                    return BadRequest(orszagok);
+                }
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            using (var context = new OlimpikonokContext())
+            {
+                try
+                {
+                    var orszagok = context.Orszags.FirstOrDefault(x => x.Id == id);
+                    if (orszagok != null)
+                    {
+                        return Ok(orszagok);
+                    }
+                    else return NotFound(new Orszag() { Id = id});
+                }
+                catch (Exception x)
+                {
+
+                    Orszag hiba = new Orszag()
+                    {
+                        Id = -1,
+                        Nev = $"Hiba az adatok betoltese kozben: {x.Message}"
+                    };
+                    return BadRequest(hiba);
+                }
+            }
+        }
+    }
+}
---
+++ 2025.11.03/olimpikonokAPI/olimpikonokAPI/Models/Orszag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace olimpikonokAPI.Models;
+
+public partial class Orszag
+{
+    public int Id { get; set; }
+
+    public string Nev { get; set; } = null!;
+
+    public string Fovaros { get; set; } = null!;
+
+    public int Nepesseg { get; set; }
+
+    public virtual ICollection<Sportolo> Sportolos { get; set; } = new List<Sportolo>();
+}
